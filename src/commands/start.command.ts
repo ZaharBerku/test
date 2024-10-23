24a3bee546b85f79ff65ddd0b8af5c9b76136534
ZaharBerku <zaharberku@gmail.com>
2024-10-23 12:00:18 +0300

    added check allowed user

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -10,57 +10,58 @@ export class StartCommand extends Command {
 
   handle(): void {
     this.bot.start(async (ctx) => {
-      console.log(ctx, "ctx");
       if (!this.isAllowedChatId(ctx)) {
-        const { id, type, title } = ctx.update.message.chat as {
-          title: string;
-          id: number;
-          type: string;
-        };
-        if (type === "supergroup") {
-          let message: string | null = null;
-          const oldChatId = ctx.payload;
-          if (oldChatId) {
-            const { data, error } = await supabase
-              .from("groups")
-              .update({ group_id: id })
-              .eq("group_id", oldChatId)
-              .select();
-            if (error) {
-              ctx.reply("Такой группы нет или она была удалена!");
-            }
-            message = await this.getStartMessageAndUpdatePaidSum(
-              id,
-              data?.at(0)?.create_at
-            );
-          } else {
-            message = this.getMeesage({ chatId: id });
-          }
-          if (message) {
-            const sentMessage = await ctx.telegram.sendMessage(id, message, {
-              parse_mode: "HTML",
-            });
-
-            await ctx.telegram.pinChatMessage(id, sentMessage.message_id);
-            if (!oldChatId) {
-              const error = await this.createGroup(
-                id,
-                title,
-                sentMessage.message_id
-              );
+        if (this.isAllowedUser(ctx)) {
+          const { id, type, title } = ctx.update.message.chat as {
+            title: string;
+            id: number;
+            type: string;
+          };
+          if (type === "supergroup") {
+            let message: string | null = null;
+            const oldChatId = ctx.payload;
+            if (oldChatId) {
+              const { data, error } = await supabase
+                .from("groups")
+                .update({ group_id: id })
+                .eq("group_id", oldChatId)
+                .select();
               if (error) {
-                ctx.reply(
-                  error.code === "23505"
-                    ? "Такая группа уже была добалена"
-                    : "Группа не была добавлена"
-                );
+                ctx.reply("Такой группы нет или она была удалена!");
               }
+              message = await this.getStartMessageAndUpdatePaidSum(
+                id,
+                data?.at(0)?.create_at
+              );
+            } else {
+              message = this.getMeesage({ chatId: id });
+            }
+            if (message) {
+              const sentMessage = await ctx.telegram.sendMessage(id, message, {
+                parse_mode: "HTML",
+              });
+
+              await ctx.telegram.pinChatMessage(id, sentMessage.message_id);
+              if (!oldChatId) {
+                const error = await this.createGroup(
+                  id,
+                  title,
+                  sentMessage.message_id
+                );
+                if (error) {
+                  ctx.reply(
+                    error.code === "23505"
+                      ? "Такая группа уже была добалена"
+                      : "Группа не была добавлена"
+                  );
+                }
+              }
+            } else {
+              ctx.reply("Что-то пошло не так!");
             }
           } else {
-            ctx.reply("Что-то пошло не так!");
+            ctx.reply("Необходимо сделать бота админом группы");
           }
-        } else {
-          ctx.reply("Необходимо сделать бота админом группы");
         }
       } else {
         ctx.reply("Эту группу нельзя добавить в список!");
